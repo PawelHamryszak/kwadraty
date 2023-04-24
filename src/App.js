3b600e98099f46985cf8513da0a4471710cfb1ec
Jakub Łączniak <71320873+Jakub-Laczniak@users.noreply.github.com>
2023-04-24 15:45:44 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,72 +1,65 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function App() {
-  const [color1, setColor1] = useState("red");
-  const [color2, setColor2] = useState("green");
-  const [color3, setColor3] = useState("blue");
-  const [color4, setColor4] = useState("yellow");
+  const [colors, setColors] = useState([]);
 
-  const handleClick1 = () => {
-    setColor1(getRandomColor());
-  };
+  useEffect(() => {
+    const colorsArr = [];
+    for (let i = 0; i < 4; i++) {
+      colorsArr.push(getRandomColor());
+    };
+    setColors(colorsArr);
+  }, []);
 
-  const handleClick2 = () => {
-    setColor2(getRandomColor());
-  };
-
-  const handleClick3 = () => {
-    setColor3(getRandomColor());
-  };
-
-  const handleClick4 = () => {
-    setColor4(getRandomColor());
+  const handleClick = (num) => {
+    const colorsArr = colors;
+    colorsArr[num] = getRandomColor();
+    setColors([...colorsArr]);
   };
 
   const getRandomColor = () => {
-    const letters = "0123456789ABCDEF";
     let color = "#";
     for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
+      color += "0123456789ABCDEF"[Math.floor(Math.random() * 16)];
     }
     return color;
   };
 
+  const tileStyle = {
+    width: "400px",
+    height: "400px",
+    display: "inline-block",
+    cursor: "pointer",
+  }
+
   return (
-    <div>
+    <div id="main-container">
       <div
-        onClick={handleClick1}
+        onClick={() => handleClick(0)}
         style={{
-          width: "100px",
-          height: "100px",
-          backgroundColor: color1,
-          margin: "2rem",
+          ...tileStyle,
+          backgroundColor: colors[0],
         }}
       ></div>
       <div
-        onClick={handleClick2}
+        onClick={() => handleClick(1)}
         style={{
-          width: "100px",
-          height: "100px",
-          backgroundColor: color2,
-          margin: "2rem",
+          ...tileStyle,
+          backgroundColor: colors[1],
         }}
       ></div>
       <div
-        onClick={handleClick3}
+        onClick={() => handleClick(2)}
         style={{
-          width: "100px",
-          height: "100px",
-          backgroundColor: color3,
-          margin: "2rem",
+          ...tileStyle,
+          backgroundColor: colors[2],
         }}
       ></div>
       <div
-        onClick={handleClick4}
+        onClick={() => handleClick(3)}
         style={{
-          width: "100px",
-          height: "100px",
-          backgroundColor: color4,
-          margin: "2rem",
+          ...tileStyle,
+          backgroundColor: colors[3],
         }}
       ></div>
     </div>
