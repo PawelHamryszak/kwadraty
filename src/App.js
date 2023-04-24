f3af550c4a2559f1c417064e85cd29bb95479ac1
Paweł hamryszak <phamryszak91@gmail.com>
2023-04-24 12:39:49 +0200

    ok

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [color1, setColor1] = useState("red");
+  const [color2, setColor2] = useState("green");
+  const [color3, setColor3] = useState("blue");
+  const [color4, setColor4] = useState("yellow");
+
+  const handleClick1 = () => {
+    setColor1(getRandomColor());
+  };
+
+  const handleClick2 = () => {
+    setColor2(getRandomColor());
+  };
+
+  const handleClick3 = () => {
+    setColor3(getRandomColor());
+  };
+
+  const handleClick4 = () => {
+    setColor4(getRandomColor());
+  };
+
+  const getRandomColor = () => {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div
+        onClick={handleClick1}
+        style={{
+          width: "100px",
+          height: "100px",
+          backgroundColor: color1,
+          margin: "2rem",
+        }}
+      ></div>
+      <div
+        onClick={handleClick2}
+        style={{
+          width: "100px",
+          height: "100px",
+          backgroundColor: color2,
+          margin: "2rem",
+        }}
+      ></div>
+      <div
+        onClick={handleClick3}
+        style={{
+          width: "100px",
+          height: "100px",
+          backgroundColor: color3,
+          margin: "2rem",
+        }}
+      ></div>
+      <div
+        onClick={handleClick4}
+        style={{
+          width: "100px",
+          height: "100px",
+          backgroundColor: color4,
+          margin: "2rem",
+        }}
+      ></div>
     </div>
   );
 }
